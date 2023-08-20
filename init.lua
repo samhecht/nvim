cb84c42fae1a42c9f9a1bff03e26ff5a81c23a32
Sam Hecht <34660188+samhecht@users.noreply.github.com>
2023-08-20 18:37:02 -0400

    initial config

--- init.lua
+++ init.lua
@@ -1,126 +1,48 @@
---[[
-
-=====================================================================
-==================== READ THIS BEFORE CONTINUING ====================
-=====================================================================
-
-Kickstart.nvim is *not* a distribution.
-
-Kickstart.nvim is a template for your own configuration.
-The goal is that you can read every line of code, top-to-bottom, understand
-what your configuration is doing, and modify it to suit your needs.
-
-Once you've done that, you should start exploring, configuring and tinkering to
-explore Neovim!
-
-If you don't know anything about Lua, I recommend taking some time to read through
-	a guide. One possible example:
-	- https://learnxinyminutes.com/docs/lua/
-
-
-	And then you can explore or search through `:help lua-guide`
-	- https://neovim.io/doc/user/lua-guide.html
-
-
-	Kickstart Guide:
-
-	I have left several `:help X` comments throughout the init.lua
-	You should run that command and read that help section for more information.
-
-	In addition, I have some `NOTE:` items throughout the file.
-	These are for you, the reader to help understand what is happening. Feel free to delete
-	them once you know what you're doing, but they should serve as a guide for when you
-	are first encountering a few different constructs in your nvim config.
-
-	I hope you enjoy your Neovim journey,
-	- TJ
-
-	P.S. You can delete this when you're done too. It's your config now :)
---]]
 -- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- Install package manager
---    https://github.com/folke/lazy.nvim
---    `:help lazy.nvim.txt` for more info
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
 	vim.fn.system {
 		'git',
 		'clone',
 		'--filter=blob:none',
-		'https://github.com/folke/lazy.nvim.git',
-		'--branch=stable', -- latest stable release
-		lazypath,
+		'git@github.com:folke/lazy.nvim.git',
+		'--branch=stable',
+		lazypath
 	}
 end
 vim.opt.rtp:prepend(lazypath)
 
--- NOTE: Here is where you install your plugins.
---  You can configure plugins using the `config` key.
---
---  You can also configure plugins after the setup call,
---    as they will be available in your neovim runtime.
 require('lazy').setup({
-	-- NOTE: First, some plugins that don't require any configuration
-
-	-- Git related plugins
 	'tpope/vim-fugitive',
 	'tpope/vim-rhubarb',
-
-	-- Detect tabstop and shiftwidth automatically
 	'tpope/vim-sleuth',
-
-	-- NOTE: This is where your plugins related to LSP can be installed.
-	--  The configuration is done below. Search for lspconfig to find it below.
-	{
-		'maxmx03/dracula.nvim',
-		lazy = false,
-		priority = 1000,
-	},
-	{
-		-- LSP Configuration & Plugins
+		{
 		'neovim/nvim-lspconfig',
 		dependencies = {
-			-- Automatically install LSPs to stdpath for neovim
 			{ 'williamboman/mason.nvim', config = true },
 			'williamboman/mason-lspconfig.nvim',
-
-			-- Useful status updates for LSP
-			-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
 			{ 'j-hui/fidget.nvim', tag = 'legacy', opts = {} },
-
-			-- Additional lua configuration, makes nvim stuff amazing!
 			'folke/neodev.nvim',
 		},
 	},
 
 	{
-		-- Autocompletion
 		'hrsh7th/nvim-cmp',
 		dependencies = {
-			-- Snippet Engine & its associated nvim-cmp source
 			'L3MON4D3/LuaSnip',
 			'saadparwaiz1/cmp_luasnip',
-
-			-- Adds LSP completion capabilities
 			'hrsh7th/cmp-nvim-lsp',
-
-			-- Adds a number of user-friendly snippets
 			'rafamadriz/friendly-snippets',
 		},
 	},
 
-	-- Useful plugin to show you pending keybinds.
 	{ 'folke/which-key.nvim', opts = {} },
 	{
-		-- Adds git related signs to the gutter, as well as utilities for managing changes
 		'lewis6991/gitsigns.nvim',
 		opts = {
-			-- See `:help gitsigns.txt`
 			signs = {
 				add = { text = '+' },
 				change = { text = '~' },
@@ -135,24 +57,23 @@ require('lazy').setup({
 			end,
 		},
 	},
-
 	{
 		-- Theme inspired by Atom
-		'navarasu/onedark.nvim',
+    -- 'navarasu/onedark.nvim',
+    'marko-cerovac/material.nvim',
 		priority = 1000,
 		config = function()
-			vim.cmd.colorscheme 'onedark'
+			vim.cmd.colorscheme 'material-palenight'
 		end,
 	},
-
+  {
+  },
 	{
-		-- Set lualine as statusline
 		'nvim-lualine/lualine.nvim',
-		-- See `:help lualine.txt`
 		opts = {
 			options = {
 				icons_enabled = false,
-				theme = 'dracula',
+				theme = 'material-stealth',
 				component_separators = '|',
 				section_separators = '',
 			},
@@ -184,8 +105,6 @@ require('lazy').setup({
 			-- requirements installed.
 			{
 				'nvim-telescope/telescope-fzf-native.nvim',
-				-- NOTE: If you are having trouble with this installation,
-				--       refer to the README for telescope-fzf-native for more instructions.
 				build = 'make',
 				cond = function()
 					return vim.fn.executable 'make' == 1
@@ -193,29 +112,14 @@ require('lazy').setup({
 			},
 		},
 	},
-
 	{
-		-- Highlight, edit, and navigate code
 		'nvim-treesitter/nvim-treesitter',
 		dependencies = {
 			'nvim-treesitter/nvim-treesitter-textobjects',
 		},
 		build = ':TSUpdate',
 	},
-
-	-- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
-	--       These are some example plugins that I've included in the kickstart repository.
-	--       Uncomment any of the lines below to enable them.
-	-- require 'kickstart.plugins.autoformat',
-	-- require 'kickstart.plugins.debug',
-
-	-- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-	--    You can use this folder to prevent any conflicts with this init.lua if you're interested in keeping
-	--    up-to-date with whatever is in the kickstart repo.
-	--    Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-	--
-	--    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
-	-- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
 }, {})
 
 -- [[ Setting options ]]
@@ -456,11 +360,12 @@ local servers = {
 	},
 }
 
+vim.cmd([[source ~/.vimrc]])
 -- Setup neovim lua configuration
 require('neodev').setup()
 
 -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
-local capabilities = vim.lsp.protocol.make_client_capabilities()
+local capabilities = vim.lsp.protocol.make_client_capabilities('python', 'ocaml', 'lua','elm')
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 -- Ensure the servers above are installed
@@ -482,65 +387,118 @@ mason_lspconfig.setup_handlers {
 }
 
 -- [[ Configure nvim-cmp ]]
--- See `:help cmp`
-local cmp = require 'cmp'
-local luasnip = require 'luasnip'
-require('luasnip.loaders.from_vscode').lazy_load()
-luasnip.config.setup {}
+-- nyinnyithan ocaml-setup google
+local status, cmp = pcall(require, "cmp")
+if (not status) then return end
 
-cmp.setup {
-	snippet = {
-		expand = function(args)
-			luasnip.lsp_expand(args.body)
-		end,
-	},
-	mapping = cmp.mapping.preset.insert {
-		['<C-n>'] = cmp.mapping.select_next_item(),
-		['<C-p>'] = cmp.mapping.select_prev_item(),
-		['<C-d>'] = cmp.mapping.scroll_docs(-4),
-		['<C-f>'] = cmp.mapping.scroll_docs(4),
-		['<C-Space>'] = cmp.mapping.complete {},
-		['<CR>'] = cmp.mapping.confirm {
-			behavior = cmp.ConfirmBehavior.Replace,
-			select = true,
-		},
-		['<Tab>'] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_next_item()
-			elseif luasnip.expand_or_locally_jumpable() then
-				luasnip.expand_or_jump()
-			else
-				fallback()
-			end
-		end, { 'i', 's' }),
-		['<S-Tab>'] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_prev_item()
-			elseif luasnip.locally_jumpable(-1) then
-				luasnip.jump(-1)
-			else
-				fallback()
-			end
-		end, { 'i', 's' }),
-	},
-	sources = {
-		{ name = 'nvim_lsp' },
-		{ name = 'luasnip' },
-	},
+local luasnip = require("luasnip")
+local lsp = require("lspconfig")
+
+local has_words_before = function()
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local cmp_kinds = {
+    Text = "[]",
+    Method = "",
+    Function = "",
+    Constructor = "",
+    Field = "",
+    Variable = "",
+    Class = "ﴯ",
+    Interface = "",
+    Module = "",
+    Property = "ﰠ",
+    Unit = "",
+    Value = "λ",
+    Enum = "",
+    Keyword = "",
+    Snippet = "",
+    Color = "",
+    File = "",
+    Reference = "",
+    Folder = "",
+    EnumMember = "",
+    Constant = "",
+    Struct = "",
+    Event = "",
+    Operator = "",
+    TypeParameter = ""
 }
 
--- The line beneath this is called `modeline`. See `:help modeline`
-vim.cmd([[set ts=2]])
-vim.cmd([[set sts=2]])
-vim.cmd([[set sw=2]])
+cmp.setup({
+    snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body)
+        end,
+    },
+    window = {
+        completion = {
+            winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
+        },
+        documentation = {
+            border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+        },
+    },
+    mapping = cmp.mapping.preset.insert({
+        ["<C-k>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-j>"] = cmp.mapping.scroll_docs(4),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<CR>"] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true
+        }),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            elseif has_words_before() then
+                cmp.complete()
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+    }),
+    sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+        { name = "buffer" },
+        option = {
+            get_bufnrs = function()
+                local bufs = {}
+                for _, win in ipairs(vim.api.nvim_list_wins()) do
+                    bufs[vim.api.nvim_win_get_buf(win)] = true
+                end
+                return vim.tbl_keys(bufs)
+            end
+        }
+    }),
+    formatting = {
+        fields = { "kind", "abbr", "menu" },
+        format = function(entry, vim_item)
+            vim_item.kind = cmp_kinds[vim_item.kind] or ""
+            local lsp_icon = "🅻"
+            if lsp ~= nil and lsp.ocamllsp ~= nil then
+                lsp_icon = "🐫"
+            end
+            vim_item.menu = ({
+                buffer = "🅱",
+                nvim_lsp = lsp_icon,
+                luasnip = "㊊"
+            })[entry.source.name]
+            return vim_item
+        end,
+    }
+})
 
--- python config
-vim.cmd([[g:python2_host_prog = '/opt/homebrew/opt/python@3.11/bin/python3']])
-
--- ocaml config
-vim.cmd([[g:opamshare = substitute(system('opam config var share'),'\n$','','''')]])
-
-vim.cmd([[execute "set rtp+=" . g:opamshare . "/merlin/vim"]])
-
-
-vim.cmd([[g:colorscheme = 'dracula']])
