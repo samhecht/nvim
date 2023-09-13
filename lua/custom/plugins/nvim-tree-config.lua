4b6853eac38bd2cfa8752d9705809231f6224dab
Sam Hecht <34660188+samhecht@users.noreply.github.com>
2023-09-13 01:20:54 -0400

    fixed file tree

--- lua/custom/plugins/nvim-tree-config.lua
+++ lua/custom/plugins/nvim-tree-config.lua
@@ -1,35 +1,7 @@
 return {
   'kyazdani42/nvim-tree.lua',
-  lazy = false,
-  config = function()
-    require 'nvim-tree'.setup {
-        side = 'left',
-        width = 40,
-        auto_resize = true,
-        select = function(_, mode)
-          if mode == 'v' then
-            vim.cmd('normal! gv')
-          end
-        end,
-        border = "shadow",
-        style = "minimal",
-        enable = true,
-        ignore = {
-          '.git',
-          'node_modules',
-          '.cache'
-      },
-      folder = {
-        arrow_open = "",
-        arrow_closed = "",
-        default = "",
-        open = "",
-        empty = "",
-        empty_open = "",
-        symlink = "",
-        symlink_open = "",
-      },
-      disable_netrw = true,
-    }
-  end
+  require 'nvim-tree'.setup {
+    disable_netrw = true,
+    hijack_netrw  = true,
+  }
 }
